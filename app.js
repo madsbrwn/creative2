54a646bfbd5a8e8ad2baeaf5767f2dec2cd13132
madelinedickson <dickson.maddy@gmail.com>
2017-10-04 14:34:29 -0600

    rewrite

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
-'use-strict';
-var url = "https://accounts.spotify.com/en/authorize?client_id=1d1477f7b6ab40648feb09704216b402&response_type=code&redirect_uri=http://cs260.madsbrwn.com/labs/lab2/weather.html&scope=user-read-private%20user-read-email&state=34fFs29kd09";
-console.log(url);
+(function() {
 
-$("#loginbtn").click(function () {
-  $.get(url, function (data) {
-    alert("it worked");
-  });
-});
+    function login(callback) {
+        var CLIENT_ID = '1d1477f7b6ab40648feb09704216b402';
+        var REDIRECT_URI = 'http://cs260.madsbrwn.com/creatives/creative2/main.html';
+        function getLoginURL(scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+              '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+              '&scope=' + encodeURIComponent(scopes.join(' ')) +
+              '&response_type=token';
+        }
 
-function createUser() {
-    return Math.random().toString(36).substr(2, 20);
-};
+        var url = getLoginURL([
+            'user-read-email'
+        ]);
+
+        var width = 450,
+            height = 730,
+            left = (screen.width / 2) - (width / 2),
+            top = (screen.height / 2) - (height / 2);
+
+        window.addEventListener("message", function(event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                callback(hash.access_token);
+            }
+        }, false);
+
+        var w = window.open(url,
+                            'Spotify',
+                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                           );
+
+    }
+
+    function getUserData(accessToken) {
+        return $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+               'Authorization': 'Bearer ' + accessToken
+            }
+        });
+    }
+
+    var templateSource = document.getElementById('result-template').innerHTML,
+        template = Handlebars.compile(templateSource),
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
+
+    loginButton.addEventListener('click', function() {
+        login(function(accessToken) {
+            getUserData(accessToken)
+                .then(function(response) {
+                    loginButton.style.display = 'none';
+                    resultsPlaceholder.innerHTML = template(response);
+                });
+            });
+    });
+
+})();
