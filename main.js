fae3c80bf73cea867edb4123e78515b0ecc2553c
madsbrwn <madelinemdickson@gmail.com>
2017-10-05 16:30:56 +0000

    working out accessing api using auth token

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var track_url = 'https://api.spotify.com/v1/me/top/tracks';
 
-$.ajax({
-  url : track_url,
-  crossDomain : true,
-  dataType : "JSON",
-  success : function(parsed_json) {
-    console.log(parsed_json);
-  }
+var token = window.location.hash.substr(1);
+var accessToken = {};
+
+$.each(token.substr(0).split('&'), function(i, elem) {
+   var parts = elem.split('=');
+   accessToken[parts[0]] = parts[1];
 });
+
+$.ajax({
+    url : track_url,
+    headers : {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Authorization': 'Bearer ' + accessToken['access_token']
+    },
+    success : function(response) {
+      console.log("it worked?");
+    }
+});
+
