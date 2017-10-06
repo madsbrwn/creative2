d002f9b194c3859490a886e51f6cf4cda202b2e7
Seth Donaldson <sethd63@gmail.com>
2017-10-05 21:06:44 -0600

    fix dumboy stuff

--- main.js
+++ main.js
@@ -14,16 +14,10 @@ var access = accessToken['access_token'];
 $.ajax({
     url : track_url,
     headers : {
-<<<<<<< HEAD
-      // 'X-Requested-With': 'XMLHttpRequest',
-      'Accept': 'application/json',
-      'Authorization': 'Bearer ' + accessToken['access_token']
-=======
       'Access-Control-Allow-Headers': 'Authorization',
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + access,
       'Accept': 'application/json'
->>>>>>> ed1374858f1e34b87948264b4c32d99583e16a2f
     },
     datatype: 'jsonp',
     success : function(parsed_json) {
